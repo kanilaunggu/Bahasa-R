806ea43fe8ff1f6ac0a919067c9b7830f0287c19
khanilaunggu <kanilaunggu@Kanis-MacBook-Pro.local>
2021-03-03 20:34:44 +0700

    speed-and-distance file and All File

---
+++ Dasar-Dasar R/operators_aritmetika.R
@@ -0,0 +1,73 @@
+#Operasi Penjumlahan
+mangga1 <- 12
+mangga2 = 13
+mangga = mangga1 + mangga2
+print(mangga)
+
+#Penjumlah Vektor
+v1 = c(12,12.2,16)
+v2 = c(2,1.2,3)
+v0 = v1 + v2
+print(v0)
+
+#Operasi Pengurangan
+mangga1 <- 16
+mangga2 = 13
+mangga = mangga1 - mangga2
+print(mangga)
+
+#Pengurangan Vektor
+v1 = c(12,12.2,16)
+v2 = c(2,1.2,3)
+v0 = v1 - v2
+print(v0)
+
+#Operasi Perkalian
+panjang <- 16
+lebar = 13
+luas = panjang * lebar
+print(luas)
+
+#Perkalian Vektor
+v1 = c(12,12.2,16)
+v2 = c(2,1.2,3)
+v0 = v1 * v2
+print(v0)
+
+#Operator Pembagian
+alas = 12.45
+tinggi = 70
+luas_segi_tiga <- (alas * tinggi) / 2
+print(luas_segi_tiga)
+
+#Pembagian Vektor
+v1 = c(12,12.2,16)
+v2 = c(2,1.2,3)
+v0 = v1 / v2
+print(v0)
+
+
+#Operator sisa Pembagian
+bil1 = 11
+bil2 = 2
+bil_sisa <- bil1 %% bil2
+print(bil_sisa)
+
+#Pembagian sisa Pembagian Vektor
+v1 = c(12,13,16)
+v2 = c(2,2,3)
+v0 = v1 %% v2
+print(v0)
+
+#Opeartor hasil pembagian %/%
+bil1 = 7
+bil2 = 6
+hasil <- bil1 %/% bil2
+print(hasil)
+
+
+
+
+
+
+
